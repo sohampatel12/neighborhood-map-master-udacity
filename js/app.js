edfc4cca4a2b728b619f882699c1100c9d98110d
Soham-Patel <sohampatel12@gmail.com>
2017-11-10 16:40:10 +0530

    search bar working

---
+++ js/app.js
@@ -0,0 +1,273 @@
+var map;
+
+var markers = [];
+
+var locations = [
+	{
+		title: 'Park Ave Penthouse',
+		location: {
+			lat: 40.7713024,
+			lng: -73.9632393
+		}
+	},
+	{
+		title: 'Chelsea Loft',
+		location: {
+			lat: 40.7444883,
+			lng: -73.9949465
+		}
+	},
+	{
+		title: 'Union Square Open Floor Plan',
+		location: {
+			lat: 40.7347062,
+			lng: -73.9895759
+		}
+	},
+	{
+		title: 'East Village Hip Studio',
+		location: {
+			lat: 40.7281777,
+			lng: -73.984377
+		}
+	},
+	{
+		title: 'TriBeCa Artsy Bachelor Pad',
+		location: {
+			lat: 40.7195264,
+			lng: -74.0089934
+		}
+	},
+	{
+		title: 'Chinatown Homey Space',
+		location: {
+			lat: 40.7180628,
+			lng: -73.9961237
+		}
+	}
+];
+
+function initMap() {
+
+	var styles = [
+		{
+			"stylers": [{
+				"saturation": -100
+			}, {
+				"gamma": 1
+			}]
+		}, {
+			"elementType": "labels.text.stroke",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "poi.business",
+			"elementType": "labels.text",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "poi.business",
+			"elementType": "labels.icon",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "poi.place_of_worship",
+			"elementType": "labels.text",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "poi.place_of_worship",
+			"elementType": "labels.icon",
+			"stylers": [{
+				"visibility": "off"
+			}]
+		}, {
+			"featureType": "road",
+			"elementType": "geometry",
+			"stylers": [{
+				"visibility": "simplified"
+			}]
+		}, {
+			"featureType": "water",
+			"stylers": [{
+				"visibility": "on"
+			}, {
+				"saturation": 50
+			}, {
+				"gamma": 0
+			}, {
+				"hue": "#50a5d1"
+			}]
+		}, {
+			"featureType": "administrative.neighborhood",
+			"elementType": "labels.text.fill",
+			"stylers": [{
+				"color": "#333333"
+			}]
+		}, {
+			"featureType": "road.local",
+			"elementType": "labels.text",
+			"stylers": [{
+				"weight": 0.5
+			}, {
+				"color": "#333333"
+			}]
+		}, {
+			"featureType": "transit.station",
+			"elementType": "labels.icon",
+			"stylers": [{
+				"gamma": 1
+			}, {
+				"saturation": 50
+			}]
+		}
+	];
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 10,
+		styles: styles,
+	});
+
+	// Earlier locations was initialised here.
+
+	var largeInfowindow = new google.maps.InfoWindow();
+
+	// Style the markers a bit. This will be our listing marker icon.
+	var defaultIcon = makeMarkerIcon('0091ff');
+
+	// Create a "highlighted location" marker color for when the user
+	// mouses over the marker.
+	var highlightedIcon = makeMarkerIcon('FFFF24');
+	// The following group uses the location array to create an array of markers on initialize.
+
+	var bounds = new google.maps.LatLngBounds();
+
+	for (var i = 0; i < locations.length; i++) {
+		// Get the position from the location array.
+		var position = locations[i].location;
+		var title = locations[i].title;
+		// Create a marker per location, and put into markers array.
+		var marker = new google.maps.Marker({
+			position: position,
+			title: title,
+			animation: google.maps.Animation.DROP,
+			icon: defaultIcon,
+			id: i
+		});
+
+		marker.setMap(map);
+		bounds.extend(marker.position);
+
+		// Push the marker to our array of markers.
+		markers.push(marker);
+		// Create an onclick event to open the large infowindow at each marker.
+		marker.addListener('click', function() {
+			populateInfoWindow(this, largeInfowindow);
+		});
+		// Two event listeners - one for mouseover, one for mouseout,
+		// to change the colors back and forth.
+		marker.addListener('mouseover', function() {
+			this.setIcon(highlightedIcon);
+		});
+		marker.addListener('mouseout', function() {
+			this.setIcon(defaultIcon);
+		});
+	}
+
+	map.fitBounds(bounds);
+
+	function makeMarkerIcon(markerColor) {
+		var markerImage = new google.maps.MarkerImage(
+			'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+			'|40|_|%E2%80%A2',
+			new google.maps.Size(21, 34),
+			new google.maps.Point(0, 0),
+			new google.maps.Point(10, 34),
+			new google.maps.Size(21, 34));
+		return markerImage;
+	}
+
+	document.getElementById('recenter-button').addEventListener('click', function() {
+		map.fitBounds(bounds);
+	});
+}
+
+function populateInfoWindow(marker, infowindow) {
+	// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		// Clear the infowindow content to give the streetview time to load.
+		infowindow.setContent('');
+		infowindow.marker = marker;
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick', function() {
+			infowindow.marker = null;
+		});
+		var streetViewService = new google.maps.StreetViewService();
+		var radius = 50;
+		// In case the status is OK, which means the pano was found, compute the
+		// position of the streetview image, then calculate the heading, then get a
+		// panorama from that and set the options
+		function getStreetView(data, status) {
+			if (status == google.maps.StreetViewStatus.OK) {
+				var nearStreetViewLocation = data.location.latLng;
+				var heading = google.maps.geometry.spherical.computeHeading(
+					nearStreetViewLocation, marker.position);
+				infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+				var panoramaOptions = {
+					position: nearStreetViewLocation,
+					pov: {
+						heading: heading,
+						pitch: 30
+					}
+				};
+				var panorama = new google.maps.StreetViewPanorama(
+					document.getElementById('pano'), panoramaOptions);
+			} else {
+				infowindow.setContent('<div>' + marker.title + '</div>' +
+					'<div>No Street View Found</div>');
+			}
+		}
+		// Use streetview service to get the closest streetview image within
+		// 50 meters of the markers position
+		streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+		// Open the infowindow on the correct marker.
+		infowindow.open(map, marker);
+	}
+}
+
+var viewModel = {
+	// var self = this;
+
+	markers : ko.observableArray([]),
+
+	locations : ko.observableArray([]),
+	searchTerm : ko.observable(''),
+
+	search: function(value) {
+		viewModel.locations.removeAll();
+		viewModel.markers.removeAll();
+
+		if(value == '') return;
+
+		for (var location in locations) {
+			if(locations[location].title.toLowerCase().indexOf(value.toLowerCase()) >=0 ) {
+				viewModel.locations.push(locations[location]);
+				viewModel.markers.push(locations[location]);
+			// } else {
+			// 	marker.setVisible(false);
+			}
+		}
+	}
+
+	// selectLocation: function(value) {
+
+	// }
+};
+
+viewModel.searchTerm.subscribe(viewModel.search);
+
+ko.applyBindings(viewModel);
